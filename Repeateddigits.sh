629d1df7fe12a48092a43da3784d42371f0f033f
memories2013 <nshivaprasad41@gmail.com>
2021-10-16 11:29:01 +0530

    Initialcommit

---
+++ Repeateddigits.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+j=0;
+
+for ((i=0; i <= 100; i++))
+do
+	echo $i;
+	if [[ $((i % 11)) -eq 0 ]]
+	then
+		arr[((j++))]=$i;
+	fi
+done
+echo $i;
+
+arr[((j))]=$((--i));
+
+echo ${arr[@]};
